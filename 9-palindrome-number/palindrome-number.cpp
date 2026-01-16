095248e593548ac3d55b93cc017d94968d94e53a
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-01-16 19:16:43 +0530

    Time: 4 ms (32.73%) | Memory: 8.4 MB (99.56%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -6,21 +6,14 @@ public:
     bool isPalindrome(int x) {
         int og=x;
      int rev=0;
-     if(x<0){
+     if(x<0 || (x%10==0 && x!=0)){
         return false;
      }
-        while(x!=0){
-            int ld=x%10;
-            if(rev>INT_MAX/10 || (rev==INT_MAX/10 && ld>7)){
-                return 0;
-            }
-            rev=rev*10+ld;
+        while(x>rev){
+            rev=rev*10+x%10;
             x=x/10;
         }
-        if(rev==og){
-            return true;
-        }
-        return false;
+        return (x==rev || x==rev/10);
     }
 };
 
