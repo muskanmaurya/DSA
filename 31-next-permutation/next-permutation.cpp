f65303ddbb65a0fcb7467137d473d1b700e71b27
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-02-02 21:06:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (91.87%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,8 +1,32 @@
 #include<algorithm>
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-    next_permutation(nums.begin(),nums.end());
+   int n=nums.size();
+   int idx=-1;
+    // Step 1: Find pivot or breakpoint from right
+   for(int i=n-2;i>=0;i--){
+    if(nums[i]<nums[i+1]){
+    idx=i;
+    break;
+   }
+   }
+//    Step 2: If no pivot, reverse whole array
+   if(idx==-1){
+    reverse(nums.begin(),nums.end());
+    return;
+   }
+
+    // Step 3: Find just larger element from right
+   for(int i=n-1;i>=idx;i--){
+    if(nums[i]>nums[idx]){
+        swap(nums[i],nums[idx]);
+        break;
+    }
+   }
+    // Step 4: Reverse suffix
+   reverse(nums.begin()+idx+1,nums.end());
     }
 
 };
