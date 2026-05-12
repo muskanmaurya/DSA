8bfb82b3633728a3873b0fd5b0c4594c8702b6a8
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-05-12 13:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (17.93%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,8 +4,15 @@ public:
     void rotate(vector<int>& nums, int d) {
         int n=nums.size();
         d=d%n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+d);
-        reverse(nums.begin()+d,nums.end());
+        vector<int> temp(n);
+    
+    // Step 1: Place each element in its new "rotated" position
+    for (int i = 0; i < n; i++) {
+        temp[(i + d) % n] = nums[i];
+    }
+    
+    // Step 2: Copy temp back to nums
+    nums = temp;
+
     }
 };
