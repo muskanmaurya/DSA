be89557ff5abc80e09c1639b559928c378346551
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-05-14 22:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.33%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        int sum=(n*(n+1))/2;
-        int s2=0;
-        for(int i=0;i<n;i++){
-            s2=s2+nums[i];
-        }
-        return sum-s2;
+    int n = nums.size();
+    // The range is [0, n], but array indices only go from 0 to n-1 so init it with n to ensure the last no of range is included.
+    int res = n; 
+    
+    for (int i = 0; i < n; i++) {
+        // Instead of two separate loops, we XOR the "Expected Number" (index i) and the "Actual Number" (nums[i]) at the same time using a ^ a = 0 (Same numbers cancel out) and 0 ^ a = a.
+        res ^= i ^ nums[i];
+    }
+    // After the loop, every number that was present in both the index pool and the array values will have canceled itself out to zero.
+    // The only value remaining in 'res' is the one that never had a pair.
+    return res;  
     }
 };
