6c4970ef8a6cf6512e5cdc0065a7de8bb55ba71b
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-05-20 18:15:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (66.61%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,31 +2,28 @@
 
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
+   void nextPermutation(vector<int>& nums) {
    int n=nums.size();
-   int idx=-1;
-    // Step 1: Find pivot or breakpoint from right
-   for(int i=n-2;i>=0;i--){
-    if(nums[i]<nums[i+1]){
-    idx=i;
-    break;
+   int pivot=-1;
+   for(int i=n-2;i>=0;i--){   //Find the breakpoint (pivot) from the right side
+    if(nums[i]<nums[i+1] ) {
+        pivot=i;
+        break; //Break immediately when you find the first peak!
+    }
    }
-   }
-//    Step 2: If no pivot, reverse whole array
-   if(idx==-1){
-    reverse(nums.begin(),nums.end());
-    return;
-   }
-
-    // Step 3: Find just larger element from right
-   for(int i=n-1;i>=idx;i--){
-    if(nums[i]>nums[idx]){
-        swap(nums[i],nums[idx]);
+    // If no pivot is found, the array is sorted in descending order (e.g., [3,2,1])
+   if (pivot == -1) {  // Just reverse the whole array to get the smallest permutation.
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+// Step 2: Find the element from the right that is strictly greater than nums[pivot]
+   for(int i=n-1;i>=pivot;i--){
+    if(nums[i]>nums[pivot]){
+        swap(nums[i],nums[pivot]); // Swap the pivot with this next greater element
         break;
     }
    }
-    // Step 4: Reverse suffix
-   reverse(nums.begin()+idx+1,nums.end());
-    }
-
+//Reverse the entire tail (everything after the pivot index)
+    reverse(nums.begin()+pivot+1,nums.end());
+   }
 };
