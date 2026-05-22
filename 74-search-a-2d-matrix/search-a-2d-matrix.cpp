56b3ecf3ec56171ac1cf789d40d42e31229a1357
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-05-22 17:32:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.00%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size();
-        for(int i=0;i<n;i++){
-            int m=matrix[i].size();
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
+
+    bool searchRow(vector<vector<int>>& matrix,int row, int target){
+        //binary search on taregt element row
+        int n = matrix[0].size(); //Column size of the target row
+        int st = 0, end = n-1; //startRowElement, endRowElement
+        while(st <= end){
+            int mid = st + (end - st) / 2; //midElement of the row
+            if (target == matrix[row][mid]) return true;
+            else if (target >= matrix[row][mid]) st = mid + 1;
+            else end = mid - 1;
         }
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //binary search on all rows 
+        int m = matrix.size(); //row size
+        int n = matrix[0].size();  //column size
+        int st = 0, end = m - 1; //startRow, endRow
+        while(st <= end){
+            int mid = st + (end - st) / 2;  //midRow
+            if(target >= matrix[mid][0] && target <= matrix[mid][n-1]){
+                //function for searching the target in the target row
+                return searchRow(matrix, mid, target); 
+            }else if(target >= matrix[mid][n-1]){
+                st = mid + 1;
+            }else {
+                end = mid - 1;
+            }
+        }  
         return false;
     }
 };
