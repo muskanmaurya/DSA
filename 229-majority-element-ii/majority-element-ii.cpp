4e9bafeb84cbce326b8a2640157ec01374d94231
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-05-28 15:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (81.83%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,23 +1,37 @@
-#include<list>
+#include<unordered_map>
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
+        //Optimal approach using Boyer-Moore voting algorithm
+        //1. The Voting Phase
         vector<int> ans;
+        // Initialize two candidates and their respective counters because there can be maximum of 2 elem whose freq > n/3
+        int cnt1=0,cnt2=0;
+        int elem1=0,elem2=0;
         for(int i=0;i<n;i++){
-            if (find(ans.begin(), ans.end(), nums[i]) != ans.end()) {
-                continue;
+           
+            if(nums[i]==elem1)cnt1++;   //Always check matching candidates first!
+            else if(nums[i]==elem2)cnt2++;
+            
+            else if(cnt1==0 && nums[i]!=elem2){  // If no match, check if a slot is completely empty
+                elem1=nums[i];
+                cnt1++;
             }
-            int cnt=0;
-            for(int j=0;j<n;j++){
-                if(nums[j]==nums[i]){
-                    cnt++;
-                }
+            else if(cnt2==0 && nums[i]!=elem1){
+                elem2=nums[i];
+                cnt2++;
             }
-            if(cnt>n/3){
-                ans.push_back(nums[i]);
-            }
+            else cnt1--,cnt2--; // If it matches neither, decrement both counters
         }
+        //2. The Verification Phase
+        cnt1=0,cnt2=0;  // Reset counters to manually verify the true frequency of survivors
+        for(int i=0;i<n;i++){
+            if(nums[i]==elem1)cnt1++;
+            else if(nums[i]==elem2)cnt2++;
+        }
+        if(cnt1>n/3) ans.push_back(elem1); // Check if their actual counts are strictly greater than n / 3
+        if(elem1!=elem2 && cnt2>n/3) ans.push_back(elem2); // Ensure el2 is distinct from el1 before verifying/adding it
         return ans;
     }
 };
