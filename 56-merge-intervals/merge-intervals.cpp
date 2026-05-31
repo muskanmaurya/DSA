5a4a26340d89d15be4d94dd80faa93b79415255e
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-05-31 17:56:13 +0530

    Time: 3 ms (89.55%) | Memory: 23.8 MB (81.63%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& nums) {
+    vector<vector<int>> merge(vector<vector<int>>& nums) {  //Optimal solution
         int n = nums.size();
-        // Step 1: Sort the intervals based on their start times.
-        sort(nums.begin(), nums.end());
-        
+        sort(nums.begin(), nums.end()); //Sort the intervals
         vector<vector<int>> ans;
-
-        // Step 2: Pick each interval one-by-one to act as a potential "anchor"
-        for (int i = 0; i < n; i++) {
-            int st = nums[i][0];
-            int end = nums[i][1];
-
-            // Step 3: CRITICAL CHECK
-            // If the below conditon meets, it means this interval has already been completely absorbed into a previous merge block. Skip it!
-            if (!ans.empty() && st <= ans.back()[1]) {
-                continue;
+        for(int i = 0; i < n; i++){
+            if(ans.empty() || nums[i][0] > ans.back()[1]){
+                // This means there is NO overlap! It's a brand new standalone interval.
+                // We safely push the entire interval pair into our answers list.
+                ans.push_back(nums[i]);
             }
-
-            // Step 4: Inner Loop
-            // Look ahead at all subsequent intervals to see how far we can stretch our current 'end' boundary.
-            for (int j = i + 1; j < n; j++) {
-                
-                // If the next interval starts before or exactly when our current boundary ends,
-                // they are overlapping!
-                if (nums[j][0] <= end) {
-                    // Update our boundary to absorb the furthermost reaching end point
-                    end = max(end, nums[j][1]);
-                } 
-                else {
-                    // Since the array is sorted, if this one doesn't overlap, no future intervals will either. Break early to save time.
-                    break;
-                }
+            //If it fails the condition above, it means the current interval DOES overlap with the last interval we saved.
+            else{
+                // Instead of pushing a new pair, we update the end boundary of our 
+                // last saved interval to stretch and absorb the larger of the two ends.
+                ans.back()[1] = max(ans.back()[1], nums[i][1]);
             }
-            
-            // Step 5: The chain broke or ended. 
-            // Push the fully expanded [start, end] pair into our final answer vector.
-            ans.push_back({st, end});
         }
-        
-        // Step 6: Return the consolidated list of non-overlapping intervals
+        // Return the fully consolidated list of non-overlapping intervals
         return ans;
     }
 };
