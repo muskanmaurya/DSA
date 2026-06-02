fb29b9749ee0d891bae6c080c6068d1eb614a1b7
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-06-02 19:42:17 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (76.64%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int t) {
-        int n=nums.size();
-        int st=0;
-        int end=n-1;
-        int ans=n;
-        while(st<=end){
-            int mid=st+(end-st)/2;
-            if(nums[mid]>=t){
-                ans=mid;
-                end=mid-1;
-            }else{
-                st=mid+1;
-            }
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int st = 0;
+        int end = n - 1;
+        while(st <= end){
+             int mid = st + (end - st) / 2;
+             //If found, return the exact index 'mid'
+             if(nums[mid] == target) return mid;
+             else if(nums[mid] > target){
+                end = mid - 1;
+             }else st = mid + 1;
         }
-        return ans;
+        //If the target is missing, the 'st' pointer naturally 
+        // lands on the correct sorted insertion index.
+        return st;
     }
 };
