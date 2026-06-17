04cddec2baca9e36e4896de80ee3f0a15894f656
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-06-17 16:11:29 +0530

    Time: 82 ms (29.43%) | Memory: 93.9 MB (64.49%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if(k == 0) return false; //edge case
         int n = nums.size();
-        unordered_map<int,int> mpp;
-        int j = 0;
+        unordered_set<int> mmw; //moving memory window
         for(int i = 0; i < n; i++){
-            if(mpp.contains(nums[i])){
-               int j = mpp[nums[i]];
-                if(abs(i - j) <= k){
-                    return true;
-                }
+            if(mmw.contains(nums[i])){
+                return true;
             }
-            mpp[nums[i]] = i;
+            else{
+                if(i >= k){ //if window size crosses k, then remove the old element  
+                    mmw.erase(nums[i - k]);
+                }
+                mmw.insert(nums[i]); //then add the new elem
+            }
         }
-        return false;
+    return false;
     }
 };
