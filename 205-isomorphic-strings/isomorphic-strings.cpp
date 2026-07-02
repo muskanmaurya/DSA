f1fba167b34ca91325285fac389ad31c619d9605
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-07-02 09:37:42 +0530

    Time: 4 ms (23.26%) | Memory: 9.4 MB (56.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -3,15 +3,20 @@ public:
     bool isIsomorphic(string s, string t) {
         // If lengths don't match, instantly false
         if(s.length() != t.length()) return false;
-        // Outer loop: pick a character
+
+        unordered_map<char, char> mapST;
+        unordered_map<char, char> mapTS;
         for(int i = 0; i < s.size(); i++){
-            // Inner loop: compare it with characters ahead of it
-            for(int j = i + 1; j < s.size(); j++){
-                // Rule 1: Same chars in 's' MUST map to same chars in 't'
-                if(s[i] == s[j] && t[i] != t[j]) return false;
-                // Rule 2: Different chars in 's' MUST NOT map to same chars in 't'
-                if(s[i] != s[j] && t[i] == t[j]) return false;
-            }
+            char cs = s[i];
+            char ct = t[i];
+
+        // Check Rule 1: Does charS already have a rule that doesn't match charT?
+            if(mapST.contains(cs) && mapST[cs] != ct) return false;
+        // Check Rule 2: Does charT already have a rule that doesn't match charS?
+            if(mapTS.contains(ct) && mapTS[ct] != cs) return false;
+
+            mapST[cs] = ct;
+            mapTS[ct] = cs;
         }
         return true;
     }
