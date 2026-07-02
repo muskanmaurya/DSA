2e436723f2f3922f59fefe17072e42f3277ee07d
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-07-02 09:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (56.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        // Optimal Solution
         // If lengths don't match, instantly false
         if(s.length() != t.length()) return false;
+        // Two arrays of size 256 initialized to 0
+        int mapST[256] = {0};
+        int mapTS[256] = {0};
 
-        unordered_map<char, char> mapST;
-        unordered_map<char, char> mapTS;
         for(int i = 0; i < s.size(); i++){
             char cs = s[i];
             char ct = t[i];
-
-        // Check Rule 1: Does charS already have a rule that doesn't match charT?
-            if(mapST.contains(cs) && mapST[cs] != ct) return false;
-        // Check Rule 2: Does charT already have a rule that doesn't match charS?
-            if(mapTS.contains(ct) && mapTS[ct] != cs) return false;
+            // If they have been mapped before, do they match the current pair?
+            if(mapST[cs] != 0 && mapST[cs] != ct) return false;
+            if(mapTS[ct] != 0 && mapTS[ct] != cs) return false;
 
             mapST[cs] = ct;
             mapTS[ct] = cs;
