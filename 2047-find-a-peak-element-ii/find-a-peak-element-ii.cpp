e358b6a529862605b1c7040c3dc33a104354b9bc
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-07-15 14:22:57 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (66.15%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
+    //helper function to find max elem in that col
+    int findMaxIdx(vector<vector<int>>& mat, int col, int m, int n){
+        int maxVal = -1;
+        int maxIdx = -1;
+        for(int i = 0; i < m; i++){
+            if(mat[i][col] > maxVal){
+                maxVal = mat[i][col];
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        //bruteforce linear search
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                int top = -1, bottom = -1, left = -1, right = -1;
-
-                if(i > 0) top = mat[i - 1][j];
-                if(i < m - 1) bottom = mat[i + 1][j];
-                if(j > 0) left = mat[i][j - 1];
-                if(j < n - 1) right = mat[i][j + 1];
-
-                if(mat[i][j] > top && mat[i][j] > bottom && mat[i][j] > left && mat[i][j] > right){
-                    return {i, j};
-                }
-            }
+        //Optimal binary search
+        int st = 0, end = n - 1;
+        while(st <= end){
+            int mid = st + (end - st) / 2;
+            int maxRowIdx = findMaxIdx(mat, mid, m, n);
+            int left = mid - 1 >= 0 ? mat[maxRowIdx][mid - 1] : -1;
+            int right = mid + 1 < n ? mat[maxRowIdx][mid + 1] : -1;
+            if(mat[maxRowIdx][mid] > left && mat[maxRowIdx][mid] > right) return {maxRowIdx, mid};
+            else if(mat[maxRowIdx][mid] < left) end = mid - 1;
+            else st = mid + 1;
         }
         return {-1, -1};
     }
