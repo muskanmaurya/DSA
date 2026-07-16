9ef9e4d29bed48162536683ea6f29dafcfde9618
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-07-16 16:27:05 +0530

    Time: 2 ms (4.15%) | Memory: 8.5 MB (23.99%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int cnt = 0;
+        int maxi = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                cnt++;
+                if(cnt > maxi) maxi = cnt;
+            }else if(s[i] == ')'){
+                cnt--;
+            }
+        }
+        return maxi;
+    }
+};
