4a8407a4f1678122a8a98eee8877599eb120b1c5
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-07-23 16:05:25 +0530

    Time: 2 ms (14.88%) | Memory: 9.1 MB (13.01%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
+        //optimal with optimization
         stringstream ss(s);
         string word;
         vector<string> words;
 
         while(ss >> word) words.push_back(word);
-
         if(pattern.length() != words.size()) return false;
+        // tracking with index  
+        unordered_map<char, int> charIdx;  //last seen position (e.g., 'a': 1)
+        unordered_map<string, int> wordIdx; //last seen position (e.g., "dog": 1)
 
-        unordered_map<char, string> charToWord;
-        unordered_map<string, char> wordToChar;
+        for(int i = 0; i < pattern.length(); i++){
+            // Check if last seen 1-based positions are equal
+            if(charIdx[pattern[i]] != wordIdx[words[i]]) return false;
+            // Store (i + 1) as the 1-based index
+            charIdx[pattern[i]] = i + 1;
+            wordIdx[words[i]] = i + 1;
 
-        for(int i = 0; i < pattern.size(); i++){
-            char c = pattern[i];
-            string w = words[i];
-
-            if(charToWord.count(c) && charToWord[c] != w) return false;
-
-            if(wordToChar.count(w) && wordToChar[w] != c) return false;
-
-            charToWord[c] = w;
-            wordToChar[w] = c;
         }
     return true;
     }
