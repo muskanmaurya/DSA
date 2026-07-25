32f38a59a2a511297d18f54544b91e1937c25ff4
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-07-25 12:41:35 +0530

    Time: 4 ms (51.55%) | Memory: 14.8 MB (56.15%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,17 +2,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int t) {
-        int n=nums.size();
-        //hashing approach
-        unordered_map<int,int> m; // Stores: {Number, Index}
-        for(int i=0;i<n;i++){
+        unordered_map<int, int> mpp;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
             int first = nums[i];
-            int second = t - first; // This is the complement we need
-            if(m.contains(second)){
-                return {m[second], i}; // Directly get value using m[second] safely
+            int second = t - first;
+            if(mpp.contains(second)){
+                return {mpp[second], i};
             }
-            m[first]=i; //inserting current number and its index in map
+            mpp[first] = i;
         }
-    return {-1, -1};
+        return {-1, -1};
     }
 };
