e4bf07994674c8ac479ad1c7cd9b772154f4b77b
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-07-26 12:26:21 +0530

    Time: 0 ms (100.00%) | Memory: 63.1 MB (11.34%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxArea(vector<int>& h) {
-        //Better
+        //Optimal
         int n = h.size();
         int maxA = 0;
         int i = 0, j = n - 1;
@@ -9,11 +9,13 @@ public:
             int w = j - i;
             int h1 = h[i];
             int h2 = h[j];
+
             int minH = min(h1, h2);
             int area = w * minH;
+
             maxA = max(maxA, area);
-            if(h1 < h2) i++;
-            else j--;
+
+            h1 < h2 ? i++ : j--;
         }
         return maxA;
     }
