e0956bb0773a545a505fb54153f96d18910f6fd6
Muskan Maurya <83282285+muskanmaurya@users.noreply.github.com>
2026-09-09 00:11:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (79.42%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int addDigits(int num) {
-        long long sum = 0;
-        while(num > 0){
-            long long ld = num % 10;
-            sum += ld;
-            num /= 10;
-        }
-        if(sum >= 10) return addDigits(sum);
-        
-        return sum;
+        if(num == 0) return 0;
+        return 1 + (num - 1) % 9;
     }
 };
